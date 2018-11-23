8133219311f0b8f82ad6bf4b2b36d59df0259a02
章昕 <zhangxin@juxinli.com>
2018-11-23 15:04:43 +0800

    fix

--- src/chapter_5/164p-12.c
+++ src/chapter_5/164p-12.c
@@ -1,15 +1,18 @@
 /*查找第k小的元素*/
 
-BSTNode *Search_Small(BSTNode*t, int k){
-    if(k<1||k>t->count)return NULL;
-    if(t->lchild==NULL){
-        if(k==1) return t;
-        else return Search_Small(t->rchild, k-1);
+BSTNode Search_Small(BSTNode*t, int k){
+    int cnt = count(root->left);
+    if (k<=cnt){
+        return Search_Small(root->left, k);
+    } else if(k>cnt+1){
+        return Search_Small(root->right, k-cnt-1);    // 减去 左子树结点数+跟结点
     }
-    else{
-        if(t->lchild->count==k-1) return t;
-        if(t->lchild->count>k-1) return Search_Small(t->lchild,k);
-        if(t->lchild->count<k-1)
-            return Search_Small(t->rchild, k-(t->lchild->count+1));
-    }
+    return root->val;
 }
+
+// 计算子树的结点个数
+int count(TreeNode *node){
+    if (!node) return 0;
+    return 1 + count(node->left) + count(node->right)
+}
+
